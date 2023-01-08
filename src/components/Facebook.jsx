86dd8232661c681d901085691f253e6b87f8ed78
Noor Mohammad <noormohammad.net1503011@gmail.com>
2023-01-09 02:15:03 +0600

    adding oauth2login package

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import { useFaceBookLoginMutation } from '../features/api/apiSlice'
 import { makeid } from '../helper/helper'
 import { toast } from 'react-toastify'
-
+import OAuth2Login from 'react-simple-oauth2-login'
 const Facebook = () => {
   const [faceBookLogin, { isLoading, error, isSuccess, isError }] =
     useFaceBookLoginMutation()
-  const responseFacebook = (response) => {
+  const onSuccess = (response) => {
     faceBookLogin({
-      userID: response.userID,
-      accessToken: response.accessToken,
+      accessToken: response.access_token,
     })
   }
   if (isLoading) {
@@ -27,12 +25,20 @@ const Facebook = () => {
       toastId: makeid(),
     })
   }
+  const onFailure = (response) => {
+    toast.error(response, {
+      toastId: makeid(),
+    })
+  }
   return (
     <div className='pb-3'>
-      <FacebookLogin
-        appId={`${process.env.REACT_APP_FACEBOOK_APP_ID}`}
-        autoLoad={false}
-        callback={responseFacebook}
+      <OAuth2Login
+        authorizationUrl='https://www.facebook.com/dialog/oauth'
+        responseType='token'
+        clientId={`${process.env.REACT_APP_FACEBOOK_APP_ID}`}
+        redirectUri='https://ultimateauth.onrender.com'
+        onSuccess={onSuccess}
+        onFailure={onFailure}
         render={(renderProps) => (
           <button
             onClick={renderProps.onClick}
@@ -42,6 +48,7 @@ const Facebook = () => {
           </button>
         )}
       />
+      ,
     </div>
   )
 }
